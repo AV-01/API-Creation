5c1abdcae2edede76a5295ee0e1c98c9345db52d
Aryavrat Mishra <105894575+AV-01@users.noreply.github.com>
2022-11-04 01:07:52 +0000

    I added stuff

--- main.py
+++ main.py
@@ -1,142 +1,61 @@
-from time import sleep
-import random
-import requests
-from colorama import Fore, Back, Style
+# from flask import Flask, jsonify
+# from threading import Thread
+# import pandas as pd
 
-doTypingEffect = True
+# event_data = pd.read_csv('events.txt')
+# print(event_data.to_string())
+# app = Flask('')
+# @app.route('/')	
+# def home():
+# 	return  "I'm alive"
 
+# @app.route('/api/add/num=<int:num1>&numt=<int:num2>', methods=['GET'])
+# def add(num1,num2):
+#   return jsonify({f"{num1}+{num2}":num1+num2})
 
-def typewritingEffect(phrase):
-  if doTypingEffect:
-    for char in phrase:
-      sleep(0.005)
-      print(char, end='', flush=True)
-  else:
-    print(phrase)
+# @app.route('/post', methods=["POST"])
+# def testpost():
+#      input_json = request.get_json(force=True) 
+#      # dictToReturn = {'text':input_json['text']}
+#      return jsonify(input_json)
 
+# def run():
+# 	app.run(host='0.0.0.0',port=7000)
 
-def randomGetter():
-  randomList = random.choice(results)
-  results.remove(randomList)
-  return randomList['question'], randomList['correct_answer'], randomList[
-    'incorrect_answers']
+# t = Thread(target=run)
+# t.start()
 
+from flask import Flask, request, jsonify
+from threading import Thread
+import pandas as pd
 
-typewritingEffect(
-  "\n\nHello, welcome to the epic quiz(better than Zach's quiz). You can tell that we're better because we have a cool printing thing which Zach doesn't know how to do.\n"
-)
+app = Flask(__name__)
+event_data = pd.read_csv('events.csv')
+user_data = pd.read_csv('users.csv')
 
+@app.route('/', methods=["POST","GET"])
+def hello_world():
+  return jsonify({"message":"Please change api url to actually get results"})
 
-def getQuestionAmount():
-  typewritingEffect("\nHow many questions do you want(limit 100): ")
-  global problems
+@app.route('/post', methods=["POST"])
+def testpost():
+  input_json = request.get_json(force=True) 
+  dictToReturn = {'text':input_json['text']}
+  return jsonify(dictToReturn)
+
+@app.route('/createUser', methods=["POST"])
+def createUser():
+  user_json = request.get_json(force=True)
+  # id= max(axis=0)
   try:
-    problems = int(input())
-  except:
-    typewritingEffect(Fore.RED +
-                      "\nPlease input a valid number. For example: 19" +
-                      Fore.WHITE)
-    getQuestionAmount()
-  if problems > 0 and problems <= 100:
-    print("\n")
-  else:
-    typewritingEffect(Fore.RED +
-                      "Please insert a whole number between 0 and 50!" +
-                      Fore.WHITE)
-    getQuestionAmount()
+    username = user_json['username']
+    password = user_json['password']
+    computer_os = user_json['computer_os']
+    add_info = add_info['add_info']
+  return jsonify({"max_id":6})
 
+def run():
+	app.run(host='0.0.0.0',port=7000)
 
-def getDifficulty():
-  global results
-  typewritingEffect(
-    "\nPlease select a difficulty and press enter \n1: Easy \n2: Medium\n3: Hard\n"
-  )
-  difficulty = input("")
-  if difficulty == "1" or difficulty.lower() == "easy":
-    results = requests.get("https://opentdb.com/api.php?amount=" +
-                           str(problems) +
-                           "&difficulty=easy&type=multiple").json()['results']
-  elif difficulty == "2" or difficulty.lower() == "medium":
-    results = requests.get(
-      "https://opentdb.com/api.php?amount=" + str(problems) +
-      "&difficulty=medium&type=multiple").json()['results']
-  elif difficulty == "3" or difficulty.lower() == "hard":
-    results = requests.get("https://opentdb.com/api.php?amount=" +
-                           str(problems) +
-                           "&difficulty=hard&type=multiple").json()['results']
-  else:
-    typewritingEffect(
-      "Sorry, that's not a valid option. Please type a number or the difficulty\n\n"
-    )
-    getDifficulty()
-
-
-getQuestionAmount()
-getDifficulty()
-questionNum = 1
-
-
-def getAnswer():
-  typewritingEffect("Please type the letter of the answer you want: ")
-  answer = input("")
-  if answer.lower() == "a" or answer.lower() == "b" or answer.lower(
-  ) == "c" or answer.lower() == "d":
-    return answer
-  else:
-    typewritingEffect(
-      Fore.RED +
-      "That's not a valid solution. Please input the letter that correlates with your answer!\n"
-      + Fore.WHITE)
-    getAnswer()
-
-
-points = 0
-
-
-def displayQuestions():
-  global points
-  global questionNum
-  necessaryItems = randomGetter()
-  typewritingEffect("\n\nQuestion #" + str(questionNum) + ": " +
-                    necessaryItems[0].replace("&quot;", "\"").replace(
-                      "&#039;", "'").replace("&amp;", ","))
-  allAnswers = necessaryItems[2]
-  allAnswers.append(necessaryItems[1])
-  random.shuffle(allAnswers)
-  typewritingEffect("\n" + Fore.RED + "A) " + allAnswers[0].replace(
-    "&quot;", "\"").replace("&#039;", "'").replace("&amp;", ",") + "\n")
-  typewritingEffect(Fore.GREEN + "B) " + allAnswers[1].replace(
-    "&quot;", "\"").replace("&#039;", "'").replace("&amp;", ",") + "\n")
-  typewritingEffect(Fore.BLUE + "C) " + allAnswers[2].replace(
-    "&quot;", "\"").replace("&#039;", "'").replace("&amp;", ",") + "\n")
-  typewritingEffect(Fore.CYAN + "D) " +
-                    allAnswers[3].replace("&quot;", "\"").replace(
-                      "&#039;", "'").replace("&amp;", ",") + "\n" + Fore.WHITE)
-  answer = getAnswer()
-  try:
-    if allAnswers[ord(answer.lower()) - 97] == necessaryItems[1]:
-      points += 1
-      typewritingEffect(Fore.GREEN + "Correct! You now have " + str(points) +
-                        " points\n\n" + Fore.WHITE)
-    else:
-      typewritingEffect(Fore.RED + "Wrong! You have " + str(points) +
-                        " points. The correct answer was \"" +
-                        necessaryItems[1].replace("&quot;", "\"").replace(
-                          "&#039;", "'").replace("&amp;", ",") + "\"\n\n" +
-                        Fore.WHITE)
-  except (AttributeError):
-    typewritingEffect(
-      Fore.RED +
-      "Sorry, there was an error in getting your answer. We'll be skipping this question since that's easier for me to program. The correct answer would've been \""
-      + necessaryItems[1].replace("&quot;", "\"").replace(
-        "&#039;", "'").replace("&amp;", ",") + "\"\n\n" + Fore.WHITE)
-  questionNum += 1
-
-
-for i in range(0, problems):
-  displayQuestions()
-typewritingEffect(Fore.GREEN +
-                  "You have finished the quiz!!!!! All code was written by " +
-                  Fore.YELLOW + "Aryavrat Mishra!" + Fore.GREEN +
-                  " Special thanks to" + Fore.RED + " Zach" + Fore.GREEN +
-                  " for inspiration.")
+t = Thread(target=run)
+t.start()
